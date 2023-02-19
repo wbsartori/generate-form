c6b0d4c183afaf495f4f559d9881ab5b9ad61fa2
wbsartori <wbsartori@gmail.com>
2023-02-19 09:44:56 -0300

    form

--- src/Core/Forms/Laravel/_form.blade.php
+++ src/Core/Forms/Laravel/_form.blade.php
@@ -1,12 +1,2 @@
-<?php
-
-if(basename($_SERVER['PHP_SELF'], '.php') === basename(__FILE__, '.php')) {
-    header('HTTP/1.0 404 Not found!');
-    die();
-}
-
-require_once BASE_ROOT . '/vendor/autoload.php';
-
-?>
 
 <h1>Form</h1>
